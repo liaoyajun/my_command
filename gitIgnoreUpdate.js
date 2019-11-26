5038764e3b24b7ed8dee27dcf4e79821e4eb62b5
liaoyajun <coderliao@163.com>
2019-11-26 16:40:10 +0800

    update

--- gitIgnoreUpdate.js
+++ gitIgnoreUpdate.js
@@ -1,8 +1,6 @@
 #! /usr/bin/env node
 'use strict';
 require('shelljs/global')
-// const argv = require('yargs').argv;
-// console.log(argv.m);
 exec('git rm -r --cached .')
 exec('git add .')
 exec('git commit -m "update .gitignore"')
---
+++ pushDev.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+'use strict';
+require('shelljs/global')
+const argv = require('yargs').argv;
+console.log(argv.m);
+exec('git add .')
+exec('git commit -m "' + argv.m + '"')
+exec('git push origin master')

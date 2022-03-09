ec6c0ab04e58fe1e58b46fb77aa97394d5f5d6ef
leo <win2@ubuntu>
2022-03-09 20:32:08 +0800

    update

--- pushDev.js
+++ pushDev.js
@@ -1,9 +1,16 @@
 #! /usr/bin/env node
 'use strict';
 require('shelljs/global')
-const argv = require('yargs').argv;
-console.log(argv.m);
+const argv = require('yargs').argv
+console.log(argv.m)
 exec('git pull origin main')
 exec('git add .')
 exec('git commit -m "' + argv.m + '"')
 exec('git push origin main')
+setInterval(function() {
+  console.log(argv.m)
+  exec('git pull origin main')
+  exec('git add .')
+  exec('git commit -m "' + argv.m + '"')
+  exec('git push origin main')
+}, 10000)
